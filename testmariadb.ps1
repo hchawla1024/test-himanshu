bdf91dab5b25a742d32fd27750373399f373d4e9
hchawla1024 <105001870+hchawla1024@users.noreply.github.com>
2023-07-06 22:04:12 +0530

    Create testmariadb.ps1

---
+++ testmariadb.ps1
@@ -0,0 +1,22 @@
+
+$name = "testwithoutpe1gp"
+$rgname = "TigerTeam-TSS_AutomatedGovernance-RG"
+$subscription = "PG-Sandbox-01"
+
+$vnetrule = Get-AzMariaDbVirtualNetworkRule -ResourceGroupName $rgname -ServerName $name
+$vnetrulename = $vnetrule.name
+
+$firewallrule = Get-AzMariaDbFirewallRule -ResourceGroupName $rgname -ServerName $name
+$firewallrulename = $firewallrule.name
+ 
+
+ if (($firewallrulename -ne $null) -and ($firewallrulename -notmatch "AllowAllWindowsAzureIps") -or ($vnetrulename -ne $null) )
+ {
+ Write-Output("The mariadb server "+ $name + " has the VNET/Firewall rules present in the server "+ $vnetrulename + " "+ $firewallrulename + ",so no action will be taken on this resource." )
+ }
+  else
+    {
+        Write-Output("Public network access would be disabled for the mariadb server " + $name + ". As there is no Firewall/Vnet/Private Endpoint is present." )
+        Set-AzResource -PropertyObject @{publicNetworkAccess="disabled"} -ResourceGroupName $rgname -ResourceType Microsoft.DBforMariaDB/servers -ResourceName $name -ApiVersion 2018-06-01 -Force
+    }
+ 
